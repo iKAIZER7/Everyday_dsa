fe6834db76bc3f6e7080f96c655cd21647d5ddd2
iKAIZER7 <ikaizer790@gmail.com>
2026-06-23 23:50:28 +0530

    new code

---
+++ Fast-Slow/findDuplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        int ptr = nums[0];
+        while (ptr != slow) {
+            ptr = nums[ptr];
+            slow = nums[slow];
+        }
+        return ptr;
+    }
+};
